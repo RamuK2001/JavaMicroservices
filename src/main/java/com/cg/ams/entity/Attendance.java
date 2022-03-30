cd033667b8f548a4cfefb07a84ef9ff0143a0479
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-30 10:43:06 +0530

    Updated Relationships

--- src/main/java/com/cg/ams/entity/Attendance.java
+++ src/main/java/com/cg/ams/entity/Attendance.java
@@ -1,37 +1,71 @@
 package com.cg.ams.entity;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.DynamicUpdate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.Date;
-
 @Entity
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 @DynamicUpdate
+
+
+
+@Table(name = "attendance")
 public class Attendance {
 	
+	
+
+
 	@Id
 	private long id;
-	private long subjectId;
-	private String subjectName;
 	
-	private long studentId;
-	private String studentName;
-	private String semester;
+	
+	
+	//private long studentId;
+	//private String studentName;
+	
 	private Date date;
 	private String totalClass;
 	private long status;
 	private long total;
 	private String percentage;
 	
-	private long courseId;
-	private String courseName;
+	public Attendance(long id, Date date, String totalClass, long status,
+			long total, String percentage) {
+		super();
+		this.id = id;
+		//this.studentId = studentId;
+		//this.studentName = studentName;
+		this.date = date;
+		this.totalClass = totalClass;
+		this.status = status;
+		this.total = total;
+		this.percentage = percentage;
+	}
+	
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	private StudentEntity student;
+	
+	
+	
+	
+	
+	
 
 }
