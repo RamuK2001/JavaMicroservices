5faf320d969c29d3624171340f129cad439f31b8
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-30 10:44:41 +0530

    Update IAttendanceService.java

--- src/main/java/com/cg/ams/service/IAttendanceService.java
+++ src/main/java/com/cg/ams/service/IAttendanceService.java
@@ -1,25 +1,22 @@
 package com.cg.ams.service;
 
-import com.cg.ams.entity.Attendance;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.cg.ams.entity.Attendance;
+import com.cg.ams.entity.StudentEntity;
 @Service
 public interface IAttendanceService {
-
-    Attendance add(Attendance entity);
-
-    void update(Attendance entity);
-
-    void delete(Attendance entity);
-
-    Attendance findByName(String student_name);
-
-    Attendance findByPk(long id);
-
-    List<Attendance> search(Attendance entity, long pageNo, int pageSize);
-
-    List<Attendance> search(Attendance entity);
+	
+	public Attendance add(Attendance entity);
+	public void update(Attendance entity);
+	public void delete(Attendance entity);
+	public Attendance findByName(String student_name);
+	public Attendance findByPk(long id);
+	//public List<StudentEntity> search(String name, int pageNo, int pageSize);
+	
+	public long count();
+	List<Attendance> getAttBySid(long sid);
 
 }
