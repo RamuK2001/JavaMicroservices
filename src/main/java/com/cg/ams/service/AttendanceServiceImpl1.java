a3fa8e0d44cafc113fa4c1ab22898f5641260928
saitejabattuwar <84036803+saitejabattuwar@users.noreply.github.com>
2022-03-30 10:45:28 +0530

    Update AttendanceServiceImpl1.java

--- src/main/java/com/cg/ams/service/AttendanceServiceImpl1.java
+++ src/main/java/com/cg/ams/service/AttendanceServiceImpl1.java
@@ -1,74 +1,90 @@
 package com.cg.ams.service;
 
-import com.cg.ams.entity.Attendance;
-import com.cg.ams.repository.IAttendanceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import com.cg.ams.entity.Attendance;
+import com.cg.ams.entity.StudentEntity;
+import com.cg.ams.exception.DataNotFoundException;
+import com.cg.ams.exception.DuplicateRecordException;
+import com.cg.ams.repository.IAttendanceRepository;
 @Component
-public class AttendanceServiceImpl implements IAttendanceService {
+public class AttendanceServiceImpl1 implements IAttendanceService {
 
-    @Autowired
-    IAttendanceRepository attRepo;
+	@Autowired
+	IAttendanceRepository attRepo;
+	
+	@Override
+	public Attendance add(Attendance entity) {
+		
+		Attendance att = attRepo.save(entity);
+		return att;
+	}
 
-    @Override
-    public Attendance add(Attendance entity) {
-        Attendance att = attRepo.save(entity);
-        att.setId(att.getId());
-        att.setSubjectId(att.getSubjectId());
-        att.setSubjectName(att.getSubjectName());
-        att.setStudentId(att.getStudentId());
-        att.setStudentName(att.getStudentName());
-        att.setSemester(att.getSemester());
-        att.setDate(att.getDate());
-        att.setTotalClass(att.getTotalClass());
-        att.setStatus(att.getStatus());
-        att.setTotal(att.getTotal());
-        att.setPercentage(att.getPercentage());
-        att.setCourseId(att.getCourseId());
-        att.setCourseName(att.getCourseName());
+	@Override
+	public void update(Attendance entity) {
+		
+		attRepo.save(entity);
+		
+		
+	}
 
-        return att;
-    }
+	@Override
+	public void delete(Attendance entity) {
+		attRepo.delete(entity);
+		
+	}
 
-    @Override
-    public void update(Attendance entity) {
+	@Override
+	public Attendance findByName(String student_name) {
+		return attRepo.findByAttName(student_name);
+		
+	}
 
-        attRepo.save(entity);
+	@Override
+	public Attendance findByPk(long id) {
+		Optional<Attendance> att = Optional.of(attRepo.getById(id));
+		return attRepo.findById(id).orElseThrow(() -> new DataNotFoundException("User not found with id: " + id));
+		
+	}
 
-    }
+	/*@Override
+	public List<StudentEntity> search(String name, int pageNo, int pageSize) {
+		Pageable currentPage = PageRequest.of(pageNo, pageSize);
 
-    @Override
-    public void delete(Attendance entity) {
-        // TODO Auto-generated method stub
-        attRepo.delete(entity);
+        return attRepo.findByStudentNameContainingAllIgnoreCase(name, currentPage);
+	}*/
 
-    }
+	@Override
+	public long count() {
+		return attRepo.count();
+	}
+	@Override
+	public List<Attendance> getAttBySid(long sid)
+	{
+		return attRepo.getAttOfSid(sid);
+	}
 
-    @Override
-    public Attendance findByName(String student_name) {
-        return attRepo.findByAttName(student_name);
+	
 
-    }
+	
 
-    @Override
-    public Attendance findByPk(long id) {
-        Attendance att = attRepo.getById(id);
-        return att;
-    }
+	
 
-    @Override
-    public List<Attendance> search(Attendance entity, long pageNo, int pageSize) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	
 
-    @Override
-    public List<Attendance> search(Attendance entity) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	
+
+	
+
+	
+	
+	 
 
 }
